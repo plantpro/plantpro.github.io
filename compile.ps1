4e5c0fdab64c4062c725072c1c8e1405eff3f0c3
DanielArgent <dtsvikevich@mail.ru>
2019-08-15 13:33:12 +0300

    automessage

--- compile.ps1
+++ compile.ps1
@@ -1,9 +1,14 @@
 <#
-	Publish debug script
-	Usage: powershell -File pubdb.ps1
+	CoffeeScript compilation file
+	Usage: powershell -File compile.ps1
+
+	Warning: with cyrillic symbols use in Windows 1251 encoding
+	Autor: Tsvikevich Denis-2019
 #>
 
+# Output path
 $libPath = "C:\\Users\\ÔÍ\\Desktop\\—œ·√¿”\\plantprotection\\scripts\\lib"
+# Input path
 $srcPath = "C:\\Users\\ÔÍ\\Desktop\\—œ·√¿”\\plantprotection\\scripts\\src"
 
 # Apply CoffeScript compiler
--- publish.ps1
+++ publish.ps1
@@ -1,12 +1,18 @@
 <#
 	Publish script
 	Usage: powershell -File publish.ps1
+
+	Warning: with cyrillic symbols use in Windows 1251 encoding
+	Autor: Tsvikevich Denis-2019
 #>
 
+# Path to project
 $projectPath = "C:\\Users\\ÔÍ\\Desktop\\—œ·√¿”\\plantprotection"
+# Path to Babel (installed globally)
 $babelPath = "C:\\Users\\ÔÍ\\AppData\\Roaming\\npm\\node_modules\\@babel"
-
+# Output path
 $libPath = "C:\\Users\\ÔÍ\\Desktop\\—œ·√¿”\\plantprotection\\scripts\\lib"
+# Input path
 $srcPath = "C:\\Users\\ÔÍ\\Desktop\\—œ·√¿”\\plantprotection\\scripts\\src"
 
 # Apply CoffeScript compiler
@@ -18,8 +24,8 @@ babel $libPath --out-dir $libPath --presets=@babel/env
 set-location $projectPath
 
 # Apply UglifyJS
-foreach ($script in get-childitem $libPath -file) {
-	uglifyjs $script.fullname -o $script.fullname
+foreach ($script in get-childitem $libPath -file | where-object { $_.extension -eq ".js" }) {
+	uglifyjs $script.fullname -o $libPath + "\\" + $script.basename + ".min.js"
 }
 
 # Publish at GitHub
