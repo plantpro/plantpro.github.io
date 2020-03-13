209b547f1899bc41fd2ae67e519c24156014b0bc
DanielArgent <dtsvikevich@mail.ru>
2020-03-13 03:59:01 +0300

    automessage

---
+++ tmp/qgis/data/plants_1.js
@@ -0,0 +1,16 @@
+var json_plants_1 = {
+"type": "FeatureCollection",
+"name": "plants_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "0", "name": "Caltha palustris", "family": "Ranunculaceae" }, "geometry": { "type": "Point", "coordinates": [ 30.378678150812661, 59.725337223352732 ] } },
+{ "type": "Feature", "properties": { "id": "1", "name": "Caltha palustris", "family": "Ranunculaceae" }, "geometry": { "type": "Point", "coordinates": [ 30.389653992648132, 59.745924248640158 ] } },
+{ "type": "Feature", "properties": { "id": "3", "name": "Trifolium repens", "family": "Fabaceae" }, "geometry": { "type": "Point", "coordinates": [ 30.392310379900856, 59.729192738179613 ] } },
+{ "type": "Feature", "properties": { "id": "4", "name": "Trifolium pratense", "family": "Fabaceae" }, "geometry": { "type": "Point", "coordinates": [ 30.39271905486282, 59.72898182456084 ] } },
+{ "type": "Feature", "properties": { "id": "5", "name": "Leucantemum vulgaris", "family": "Astraceae" }, "geometry": { "type": "Point", "coordinates": [ 30.387600887482073, 59.732155198211323 ] } },
+{ "type": "Feature", "properties": { "id": "6", "name": "Geum rivale", "family": "Rosaceae" }, "geometry": { "type": "Point", "coordinates": [ 30.377340226830039, 59.725383825740238 ] } },
+{ "type": "Feature", "properties": { "id": "8", "name": "Caltha palustris", "family": "Ranunculaceae" }, "geometry": { "type": "Point", "coordinates": [ 30.370773681116241, 59.723603274858249 ] } },
+{ "type": "Feature", "properties": { "id": "9", "name": "Caltha palustris", "family": "Ranunculaceae" }, "geometry": { "type": "Point", "coordinates": [ 30.370429302467333, 59.723692718455261 ] } },
+{ "type": "Feature", "properties": { "id": "20", "name": "Caltha palustris", "family": "Ranunculaceae" }, "geometry": { "type": "Point", "coordinates": [ 30.370930216865741, 59.723871604931745 ] } }
+]
+}
