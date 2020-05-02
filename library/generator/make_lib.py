2e13896465d28d6f987c7369914dbdc9a9f9ebec
DanielArgent <dtsvikevich@mail.ru>
2020-05-02 20:27:57 +0300

    automessage

--- library/generator/make_lib.py
+++ library/generator/make_lib.py
@@ -2,180 +2,100 @@ import csv
 from pathlib import Path
 
 CATALOG_NAME = "library\\generator\\catalog.csv"
-OUTPUT_PATH = "library\\index.html"
+OUTPUT_PATH = Path("library\\index.html")
 
-STYLES = """
-	#record-count {
-		color: #999999;
-    font-size: smaller;
-	}
+FILE_TYPE_COLORS = {
+	"pdf": "rgb(231, 47, 47)",
+	"djvu": "rgb(160, 0, 160)",
+	"online": "rgb(112, 112, 112)"
+}
 
-	.plpro-lib-record-name {
-		margin-bottom: 0px;
-	}
-
-	.plpro-lib-record-year, .plpro-lib-record-autor {
-		color: #999999;
-    font-size: smaller;
-	}
-
-	.plpro-lib-record-autor {
-		text-decoration: underline;
-    cursor: pointer;
-	}
-
-	.mdl-chip {
-		color: #fff;
-		background-color: rgb(0, 129, 32);
-	}
-
-	.mdl-chip__text {
-		font-size: small;
-    vertical-align: middle;
-    display: inline-block;
-	}
-
-	.mdl-chip__action {
-		opacity: 1;
-	}
-"""
-
-CLOSE_BUTTON = """<svg style='width:18px;height:18px' viewBox='0 0 24 24'><path fill='#ffffff' d='M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z' /></svg>"""
-
-def getRecords():
+def get_records():
 	with open(CATALOG_NAME, encoding="utf-8") as file:
 		reader = csv.DictReader(file, delimiter=',')
 		records = [record for record in reader]
 	return records
 
-def getFileTypeColor(filetype):
-	if filetype == "pdf":
-		return "rgb(231, 47, 47)"
-	if filetype == "djvu":
-		return "rgb(160, 0, 160)"
-	if filetype == "online":
-		return "rgb(112, 112, 112)"
+def autor_format(autor):
+	return f"""<span class="plpro-lib-record-autor" onclick="autorOnClick(this);">{autor}</span>"""
 
-def htmlize(record):
-	fileTypeColor = getFileTypeColor(record["Тип файла"])
-	result = f"""
-		<div class="plpro-lib-record">
-			<h6 class="plpro-lib-record-name">
-				<a href="{record["Ссылка"]}">{record["Название на языке оригинала"]}</a>
-				<span class="filetype-tag" style="background-color: {fileTypeColor};">{record["Тип файла"]}</span>
-			</h6>
-			<span class="plpro-lib-record-year">{record["Год издания"]}</span>
+def record_to_html(record):
+	return f"""
+	<div class="plpro-lib-record">
+		<h6 class="plpro-lib-record-title">
+			<a href="{record["Ссылка"]}">{record["Название на языке оригинала"]}</a>
+			<span class="filetype-tag" style="background-color: {FILE_TYPE_COLORS[record["Тип файла"]]};">
+				{record["Тип файла"]}
+			</span>
+		</h6>
+		<span class="plpro-lib-record-year">{record["Год издания"]}</span>
+		{" ".join(map(autor_format, record["Авторы"].split(", ")))}
+		<span class="plpro-lib-record-size">{record["Размер"]}</span>
+	</div>
 	"""
-
-	for autor in record["Авторы"].split(", "):
-		result += f"<span class='plpro-lib-record-autor' onclick='onAutorClick(this);'>{autor}</span> "
-
-	return result + "</div>"
 
 def writeToFile(content, recordsCount):
-	output_file = Path(OUTPUT_PATH)
-
 	result = f"""
-<!doctype html>
-<html lang="ru">
+		<!doctype html>
+		<html lang="ru">
+			<head>
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Библиотека</title>
 
-	<title>Библиотека</title>
+				<link rel="stylesheet" href="../stylesheets/material.min.css">
+				<script src="../scripts/material.min.js"></script>
 
-	<link rel="stylesheet" href="../stylesheets/material.min.css">
-	<script src="../scripts/material.min.js"></script>
+				<link rel="stylesheet" href="../stylesheets/library.css">
+				<script src="../scripts/lib/lib-index.min.js"></script>
+			</head>
 
-	<style>
-		{STYLES}
-	</style>
+			<body>
+				<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+					<header class="mdl-layout__header">
+						<div class="mdl-layout__header-row">
+							<div class="mdl-layout__drawer-button">
+								<i class="material-icons">
+									<svg style="width:24px;height:24px" viewbox="0 0 24 24">
+										<path fill="rgb(0, 129, 32)"
+											d="M17,8C8,10 5.9,16.17 3.82,21.34L5.71,22L6.66,19.7C7.14,19.87 7.64,20 8,20C19,20 22,3 22,3C21,5 14,5.25 9,6.25C4,7.25 2,11.5 2,13.5C2,15.5 3.75,17.25 3.75,17.25C7,8 17,8 17,8Z" />
+									</svg>
+								</i>
+							</div>
 
-	<script>
-		function updateFiltre(text) {{
-			var filters = document.getElementById("filters");
-			filters.innerHTML = "<span class='mdl-chip mdl-chip--deletable'><span class='mdl-chip__text'>" + text + "</span>" +
-    	"<button type='button' class='mdl-chip__action' onclick='clearFiltres()'>{CLOSE_BUTTON}</button></span>"
-		}}
+							<span class="mdl-layout-title">
+								<span style="color: rgb(0, 129, 32);">P</span>LANT
+								<span style="color: rgb(0, 129, 32);">P</span>ROTECTION
+							</span>
 
-		function clearFiltres() {{
-			var filtres = document.getElementById("filters");
-			filtres.innerText = "";
+							<div class="mdl-layout-spacer"></div>
+							<nav class="mdl-navigation mdl-layout--large-screen-only">
+							</nav>
+						</div>
+					</header>
 
-			for(var i of document.getElementById("search-box").children) {{
-				if (i.className == "plpro-lib-record") {{
-					i.style.display = "block";
-				}}
-			}}
-		}}
+					<main class="mdl-layout__content">
+						<div class="page-content paddinget" id="search-box">
+							<div style="text-align: center; margin-bottom: 15px;">
+								<h3 style="margin-bottom: 0px;">Библиотека</h3>
+								<span id="record-count">Записей в библиотеке: {recordsCount}</span>
+							</div>
 
-		function searchAutor(text) {{
-			for(var i of document.getElementById("search-box").children) {{
-				if (i.className == "plpro-lib-record") {{
-					i.style.display = "none";
-					for(var j of i.children) {{
-						if (j.className == "plpro-lib-record-autor" && j.innerText == text) {{
-							i.style.display = "block";
-						}}
-					}}
-				}}
-			}}
-		}}
+							<div id="filter"></div>
 
-		function onAutorClick(self) {{
-			searchAutor(self.innerText);
-			updateFiltre("Автор: " + self.innerText);
-		}}
-	</script>
-</head>
-
-<body>
-	<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-		<header class="mdl-layout__header">
-			<div class="mdl-layout__header-row">
-				<div class="mdl-layout__drawer-button">
-					<i class="material-icons">
-						<svg style="width:24px;height:24px" viewbox="0 0 24 24">
-							<path fill="rgb(0, 129, 32)"
-								d="M17,8C8,10 5.9,16.17 3.82,21.34L5.71,22L6.66,19.7C7.14,19.87 7.64,20 8,20C19,20 22,3 22,3C21,5 14,5.25 9,6.25C4,7.25 2,11.5 2,13.5C2,15.5 3.75,17.25 3.75,17.25C7,8 17,8 17,8Z" />
-						</svg>
-					</i>
+							{content}
+						</div>
+					</main>
 				</div>
-
-				<span class="mdl-layout-title">
-					<span style="color: rgb(0, 129, 32);">P</span>LANT
-					<span style="color: rgb(0, 129, 32);">P</span>ROTECTION
-				</span>
-
-				<div class="mdl-layout-spacer"></div>
-				<nav class="mdl-navigation mdl-layout--large-screen-only">
-				</nav>
-			</div>
-		</header>
-
-		<main class="mdl-layout__content">
-			<div class="page-content paddinget" id="search-box">
-				<div style="text-align: center; margin-bottom: 15px;">
-					<h3 style="margin-bottom: 0px;">Библиотека</h3>
-					<span id="record-count">Записей в библиотеке: {recordsCount}</span>
-				</div>
-
-				<div id="filters"></div>
-
-				{content}
-			</div>
-		</main>
-	</div>
-	</body>
-
-	</html>
+				</body>
+			</html>
 	"""
-	output_file.write_text(result, encoding="utf-8")
+	OUTPUT_PATH.write_text(result, encoding="utf-8")
 
 def main():
-	records = getRecords()
-	content = "".join(htmlize(record) for record in records)
+	records = get_records()
+	content = "".join(map(record_to_html, records))
 	writeToFile(content, len(records))
 
 main()
---
+++ scripts/src/lib-index.coffee
@@ -0,0 +1,36 @@
+#: import flexibel.coffee
+
+makeChip = (text) -> "
+		<span class='mdl-chip mdl-chip--deletable'>
+			<span class='mdl-chip__text'>#{text}</span>
+			<button type='button' class='mdl-chip__action' onclick='clearFilter()'>
+				<svg style='width:18px;height:18px' viewBox='0 0 24 24'>
+					<path fill='#ffffff' d='M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z' />
+				</svg>
+			</button>
+		</span>
+	"
+
+updateFilter = (text) ->
+	htmlset "filter", makeChip text
+
+clearFilter = () ->
+	htmlset "filter", ""
+
+	searchBox = element "search-box"
+
+	for i in searchBox.children
+		i.style.display = "block" if i.className == "plpro-lib-record"
+
+searchAutor = (text) ->
+	searchBox = element "search-box"
+	for i in searchBox.children
+		if i.className == "plpro-lib-record"
+			i.style.display = "none"
+			for j in i.children
+				if j.className == "plpro-lib-record-autor" and j.innerText == text
+					i.style.display = "block"
+
+autorOnClick = (self) ->
+	searchAutor self.innerText
+	updateFilter "Автор: #{self.innerText}"
