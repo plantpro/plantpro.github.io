111c1c0f53072bbd96363475fa62e92db4511a27
DanielArgent <dtsvikevich@mail.ru>
2020-08-13 00:41:09 +0300

    automessage

--- scripts/lib/lib-index.js
+++ scripts/lib/lib-index.js
@@ -435,11 +435,11 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
   };
 
   makeChip = function makeChip(text, num) {
-    return "<span class='panel'> <span class='mdl-chip__text'>".concat(text, "</span> <button type='button' class='mdl-chip__action' onclick='document.clearFilter(this, ").concat(num, ")'> <svg style='width:18px;height:18px' viewBox='0 0 24 24'> <path fill='#ffffff' d='M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z' /> </svg> </button> </span>");
+    return "<div class='panel'> <span class='mdl-chip__text'>".concat(text, "</span> <button type='button' class='close-panel-btn' onclick='document.clearFilter(this, ").concat(num, ")'> <svg style='width:18px;height:18px' viewBox='0 0 24 24'> <path fill='#ffffff' d='M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z' /> </svg> </button> </div>");
   };
 
   makeChipWithColor = function makeChipWithColor(text, color, num) {
-    return "<span class='mdl-chip mdl-chip--deletable' style='background-color: ".concat(color, "'> <span class='mdl-chip__text'>").concat(text, "</span> <button type='button' class='mdl-chip__action' onclick='document.clearFilter(this, ").concat(num, ")'> <svg style='width:18px;height:18px' viewBox='0 0 24 24'> <path fill='#ffffff' d='M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z' /> </svg> </button> </span>");
+    return "<div class='panel'> <span class='mdl-chip__text'>".concat(text, "</span> <button type='button' class='mdl-chip__action' onclick='document.clearFilter(this, ").concat(num, ")'> <svg style='width:18px;height:18px' viewBox='0 0 24 24'> <path fill='#ffffff' d='M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z' /> </svg> </button> </div>");
   };
 
   clearFilter = function clearFilter(self, num) {
